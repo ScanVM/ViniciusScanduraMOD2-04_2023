17f891f6513bab0db7bc5763816d727dbf3fbb25
Vinicius Scandura <viniciusscandura@gmail.com>
2023-04-14 13:35:17 -0300

    pulo caindo rápido

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -17,7 +17,7 @@ class Dinosaur:
         self.dino_jump = False
         self.jump_vel = self.JUMP_VEL
         self.dino_ducking = False
-        
+        self.verification_key_down = False
 
     def update(self, user_input):
         if self.dino_run:
@@ -37,12 +37,17 @@ class Dinosaur:
         elif not self.dino_jump:
             self.dino_jump = False
             self.dino_run = True
-            self.dino_ducking = False
+            self.verification_key_down = False
             
-        if user_input[pygame.K_DOWN] and not self.dino_ducking and not self.dino_jump:
+        if user_input[pygame.K_DOWN]:
+            if not self.verification_key_down:
                 self.dino_ducking = True
+                self.verification_key_down = True
                 self.dino_run = False
                 self.dino_jump = False
+            else:
+                self.verification_key_down = False
+
 
     def run(self):
         self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
@@ -68,6 +73,7 @@ class Dinosaur:
             self.dino_rect = self.image.get_rect()
             self.dino_rect.x = self.X_POS
             self.dino_rect.y = self.Y_POS + 40
+            self.jump_vel = self.JUMP_VEL
             self.step_index += 1
 
     def draw(self, screen: pygame.Surface):
